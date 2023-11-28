23beb9cba7efcbb815d1410cb7e847e1499c3e74
ruonanzh <roronoazaoxl@gmail.com>
2023-11-28 13:43:16 +0800

    Add Personal Lora Back

--- app/src/components/AgentEdit.tsx
+++ app/src/components/AgentEdit.tsx
@@ -10,6 +10,7 @@ import {
 import { AgentDef } from "client";
 import { useState } from "react";
 import { Action } from "client";
+import { Memory } from "client";
 import AgentActionEditor from "./AgentActionEditor";
 import AgentLore from "./AgentLore";
 import AgentCoreBio, { AgentCoreBioSubset } from "./AgentCoreBio";
@@ -22,6 +23,7 @@ interface AgentEditProps {
 export default function AgentEdit(props: AgentEditProps) {
   // So the actions doesn't keep reloading.
   const [actions, setActions] = useState<Action[]>(props.agent.actions ?? []);
+  const [personal_lore, setPersonalLore] = useState<Memory[]>(props.agent.personal_lore ?? []);
 
   const saveCoreBio = async (subset: AgentCoreBioSubset) => {
     await props.handleSave({ ...props.agent, ...subset });
@@ -31,6 +33,12 @@ export default function AgentEdit(props: AgentEditProps) {
     setActions(actions);
     await props.handleSave({ ...props.agent, actions });
   };
+
+  const savePersonMemory = async (personal_lore: Memory[]) => {  
+    setPersonalLore(personal_lore);
+    await props.handleSave({ ...props.agent, personal_lore });
+  }
+
 
   return (
     <Tabs bg="white" rounded="md" width="100%">
@@ -47,7 +55,7 @@ export default function AgentEdit(props: AgentEditProps) {
           <AgentCoreBio agent={props.agent} handleSave={saveCoreBio} />
         </TabPanel>
         <TabPanel>
-          <AgentLore agentUuid={props.agent.uuid!} />
+          <AgentLore agentUuid={props.agent.uuid!} agent={props.agent} handleSave={savePersonMemory} />
         </TabPanel>
         <TabPanel>
           <AgentActionEditor
--- app/src/components/AgentLore.tsx
+++ app/src/components/AgentLore.tsx
@@ -1,24 +1,69 @@
 import {
   Heading,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Flex,
+  Center,
+  Editable,
+  EditableTextarea,
   IconButton,
   ListItem,
   Text,
   Tooltip,
   UnorderedList,
   VStack,
+  HStack,
 } from "@chakra-ui/react";
-import { InfoIcon } from "@chakra-ui/icons";
-import { GameDefinitionsService, Lore } from "client";
+import { AddIcon, InfoIcon } from "@chakra-ui/icons";
+import { GameDefinitionsService, Lore, AgentDef, Memory } from "client";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import DeleteModal from "util/DeleteModal";
+import { ColoredPreview } from "util/ColoredPreview";
 
-interface LoreProps {
+interface AgentLoreProps {
   agentUuid: string;
+  agent: AgentDef;
+  handleSave: (personalMemory: Memory[]) => Promise<void>;
 }
 
-export default function AgentLore(props: LoreProps) {
+export default function AgentLore(props: AgentLoreProps) {
   const params = useParams();
   const [sharedLore, setSharedLore] = useState<Lore[]>([]);
+  const [personalMemory, setMemoryLore] = useState<Memory[]>([]);
+
+  const handleDelete = (index: number) => {
+    setMemoryLore(oldItems => {
+      const newItems = [...oldItems];
+      newItems.splice(index, 1);
+      props.handleSave(newItems);
+      return newItems;
+    });
+  };
+  const handleClintIDChange = (index: number, newClientID: string) => {
+    setMemoryLore(oldItems => {
+      const newItems = [...oldItems];
+      newItems[index].client_id = newClientID;
+      return newItems;
+    });
+  }
+
+  const handleNullCheck = (value: string | undefined) => {
+    if (value === undefined || value === null) {
+      return "";
+    }
+    return value;
+  }
+
+  const handleDescriptionChange = (index: number, newDescription: string) => {
+    setMemoryLore(oldItems => {
+      const newItems = [...oldItems];
+      newItems[index].description = newDescription;
+      return newItems;
+    });
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,9 +72,11 @@ export default function AgentLore(props: LoreProps) {
         (lore.known_by ?? []).includes(props.agentUuid),
       );
       setSharedLore(filtered);
+      const temp = props.agent.personal_lore ?? [];
+      setMemoryLore(temp);
     };
     fetchData();
-  }, [params.gameUuid, props.agentUuid]);
+  }, [params.gameUuid, props.agentUuid, props.agent.personal_lore]);
 
   return (
     <VStack spacing={4} align={"left"}>
@@ -54,7 +101,66 @@ export default function AgentLore(props: LoreProps) {
         ))}
       </UnorderedList>
       <Heading size="lg">Personal Lore</Heading>
-      <Text>Deprecated - use shared lore with just 1 Agent.</Text>
+      <Text>Personal memory: </Text>
+      <Text>比如：塑造了角色的儿时噩梦.</Text>
+      <Card
+          onClick={() =>
+            setMemoryLore(oldMemory => [
+              { description: "Click to edit!", importance: 10  },
+              ...oldMemory,
+            ])
+          }
+          style={{ cursor: "pointer" }}
+        >
+          <CardBody>
+            <Flex flexDirection="column" justify="center" h="100%">
+              <CardHeader>
+                <Center>
+                  <Heading size="lg">
+                    New Lore
+                    <AddIcon marginLeft={3} />
+                  </Heading>
+                </Center>
+              </CardHeader>
+            </Flex>
+          </CardBody>
+        </Card>
+        {personalMemory.map((memory, index) => (
+          <Card width={"100%"} key={index}>
+            <CardBody>
+              <Editable
+                width={"full"}
+                height="max-content"
+                key={index}
+                value={handleNullCheck(memory.client_id)}
+                onChange={value => handleClintIDChange(index, value)}
+                onSubmit={() => props.handleSave(personalMemory)}
+              >
+                <ColoredPreview minHeight={10} />
+                <EditableTextarea value={memory.client_id} />
+              </Editable>
+              <HStack marginBottom={5}>
+                <Editable
+                  width={"full"}
+                  height="max-content"
+                  key={index}
+                  value={memory.description}
+                  onChange={value => handleDescriptionChange(index, value)}
+                  onSubmit={() => props.handleSave(personalMemory)}
+                >
+                  <ColoredPreview minHeight={10} />
+                  <EditableTextarea value={memory.description} />
+                </Editable>
+                <Box marginTop={"-8px"}>
+                      <DeleteModal
+                        text="Are you sure you want to delete this shared lore?"
+                        onDelete={() => handleDelete(index)}
+                      ></DeleteModal>
+                </Box>
+              </HStack>
+            </CardBody>
+          </Card>  
+        ))}
     </VStack>
   );
 }
